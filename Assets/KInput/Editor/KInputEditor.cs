1c6ce798a854658f852d334d344f22c3b69995ef
¨KasperHdL¨ <kasper@honnens.dk>
2016-11-01 08:30:25 +0100

    Initial

---
+++ Assets/KInput/Editor/KInputEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class KInputEditor : MonoBehaviour {
+
+    public static bool clearInputManager = true;
+
+
+
+
+    [MenuItem ("KInput/SetUpInputManager")]
+    static void SetUpInputManager(){
+
+    }
+}
---
+++ Assets/KInput/KInputTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KInputTest : MonoBehaviour {
+    KInput.Controller controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = new KInput.Xbox360();
+	}
+	
+	// Update is called once per frame
+    void Update () {
+        for(int i = 0; i < (int) KInput.Button.Count; i++){
+            Debug.Log("Button " + i + " - " + ((KInput.Button)i) + " :" + controller.GetButton((KInput.Button)i));
+        }
+        
+        for(int i = 0; i < (int) KInput.Axis.Count; i++){
+            Debug.Log("Axis " + i + " - " + ((KInput.Axis)i) + " :" + controller.GetAxis((KInput.Axis)i));
+        }
+    }
+}
